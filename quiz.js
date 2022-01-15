0ada7f03bd91519ef265e32758484ad8c21d8029
Raghav Bang <raghavbang2012@gmail.com>
2022-01-15 15:08:58 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ const output = document.querySelector(".outputmarks")
 
 const ans = ["90", "right angled","Equilateral","85","no"]
 
-function checkAns() {
+const checkAns=()=> {
     let score = 0
     let index = 0
     const formResult = new FormData(quizform)
@@ -15,6 +15,6 @@ function checkAns() {
         }
         index += 1
     }
-    output.innerText = "Your score is " + score
+    output.innerText = `Your score is ${score}`
 }
 btn.addEventListener("click", checkAns, false)
