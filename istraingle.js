2d9695c4f41f3d267b2a8f1dd6813f2c4b3a7644
Raghav Bang <raghavbang2012@gmail.com>
2022-01-15 15:08:11 +0530

    Update istraingle.js

--- istraingle.js
+++ istraingle.js
@@ -2,17 +2,17 @@ const inputs = document.querySelectorAll(".angle-input")
 const istriangle = document.querySelector(".is-traingle")
 const output = document.querySelector("#output")
 
-function checkTraingle() {
+const checkTraingle=() =>{
     if(inputs[0].value>0 && inputs[1].value>0 && inputs[2].value>0){
-    var sum = Number(inputs[0].value) + Number(inputs[1].value) + Number(inputs[2].value)
+    let sum = Number(inputs[0].value) + Number(inputs[1].value) + Number(inputs[2].value)
     if (sum === 180) {
-        output.innerText = "Yay, It's a traingle"
+        output.innerText = `Yay, It's a traingle`
     } else {
-        output.innerText = "Oh no, It is not a triangle"
+        output.innerText = `Oh no, It is not a triangle`
     }
     }
     else{
-        output.innerText ="Enter positive number"
+        output.innerText =`Enter positive number`
     }
 }
 istriangle.addEventListener("click", checkTraingle, false)
